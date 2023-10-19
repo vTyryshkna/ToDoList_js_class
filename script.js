a5b0c4a21eb0874dc8d25420a9dc19eda0a81125
Viktoriia Pobegailo <tyryshkinaya@gmail.com>
2023-10-19 23:33:26 +0700

    edited in the script: +domcontent loaded and classes changed

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-const inputElement = document.querySelector('.input');
-const buttonElement = document.querySelector('.button');
-const listTasks = document.querySelector('.taskList');
 const tagCSS = "text-decoration";
 const styleCSS_forText = "line-through";
 
-class AddToDoList {
-
-    constructor (containerName) {
-        this.createdElmDiv(containerName);
-    } 
+class ToDoList {
 
     createdElmDiv (classNameString) {
         const divObject = document.createElement("div");
@@ -18,11 +11,7 @@ class AddToDoList {
     }
 }
 
-class AddTaskList {
-
-    constructor (name, typeAtribute, valueAtribute, editingAccess) {
-        this.createdElmTask(name, typeAtribute, valueAtribute, editingAccess);
-    }
+class TaskList {
 
     createdElmTask (name, typeAtribute, valueAtribute, editingAccess) {
         const task = document.createElement("input");
@@ -35,134 +24,137 @@ class AddTaskList {
     }
 }
 
-class AddControlButtons {
+class ControlButtons {
 
-    constructor (path, typeButton, item, task) {
-        this.createdElmImg(path, typeButton, item, task);
-    }
     createdElmImg (path, typeButton, item, task) {
         const img = document.createElement("img");
         img.src = path;
-        if (typeButton === "edit") {
-            img.onclick = editText(img, item);
+        if (typeButton === "edit") {//переделать на switch/case
+            img.onclick = this.editText(img, item);
         }
         else if (typeButton === "save") {
-            img.onclick = saveText(img, item);
+            img.onclick = this.saveText(img, item);
         } 
         else if (typeButton === "completed") {
-            img.onclick = crossOutText(img, item);
+            img.onclick = this.crossOutText(img, item);
         }
         else if (typeButton === "delete") {
-            img.onclick = deleteText(img, task);
+            img.onclick = this.deleteText(img, task);
         }
 
         return img;
     }
-}
 
-buttonElement.addEventListener("click", () => {
-    if (inputElement.value === "") {
-        inputElement.style.borderColor = "#800000";
-        alert("You didn't fill in this field!");
-    } 
-    else {
-        //контейнер для текста
-        let newTask = new AddToDoList("taskText");
-        newTask = newTask.createdElmDiv("taskText");
-        listTasks.appendChild(newTask);
-
-        //создание задания
-        let newItem = new AddTaskList("task", "text", inputElement.value, "disabled");
-        newItem = newItem.createdElmTask("task", "text", inputElement.value, "disabled");
-        newTask.appendChild(newItem);
-
-        //создание кнопок управления
-        let newIcons_edit = new AddControlButtons("../to-do list/img/edit.png", "edit", newItem, newTask);
-        newIcons_edit = newIcons_edit.createdElmImg("../to-do list/img/edit.png", "edit", newItem, newTask);
-        newTask.appendChild(newIcons_edit);
-
-        let newIcons_save = new AddControlButtons("../to-do list/img/save.png", "save", newItem, newTask);
-        newIcons_save = newIcons_save.createdElmImg("../to-do list/img/save.png", "save", newItem, newTask);
-        newTask.appendChild(newIcons_save);
-
-        let newIcons_ready = new AddControlButtons("../to-do list/img/task_icon.png", "completed", newItem, newTask);
-        newIcons_ready = newIcons_ready.createdElmImg("../to-do list/img/task_icon.png", "completed", newItem, newTask);
-        newTask.appendChild(newIcons_ready);
-
-        let newIcons_delete = new AddControlButtons("../to-do list/img/delete.png", "delete", newItem, newTask);
-        newIcons_delete = newIcons_delete.createdElmImg("../to-do list/img/delete.png", "delete", newItem, newTask);
-        newTask.appendChild(newIcons_delete);
-
-        //отчистим input
-        inputElement.value = "";
+    //ФУНКЦИИ
+    editText (icons_edit, newItem) {
+        icons_edit.addEventListener('click', () => {
+            newItem.disabled = "";
+            newItem.focus();
+        });
     }
-});
 
-buttonElement.removeEventListener("click", () => {
-    if (inputElement.value === "") {
-        inputElement.style.borderColor = "#800000";
-        alert("You didn't fill in this field!");
-    } 
-    else {
-        //контейнер для текста
-        let newTask = new AddToDoList("taskText");
-        newTask = newTask.createdElmDiv("taskText");
-        listTasks.appendChild(newTask);
-
-        //создание задания
-        let newItem = new AddTaskList("task", "text", inputElement.value, "disabled");
-        newItem = newItem.createdElmTask("task", "text", inputElement.value, "disabled");
-        newTask.appendChild(newItem);
-
-        //создание кнопок управления
-        let newIcons_edit = new AddControlButtons("../to-do list/img/edit.png", "edit", newItem, newTask);
-        newIcons_edit = newIcons_edit.createdElmImg("../to-do list/img/edit.png", "edit", newItem, newTask);
-        newTask.appendChild(newIcons_edit);
-
-        let newIcons_save = new AddControlButtons("../to-do list/img/save.png", "save", newItem, newTask);
-        newIcons_save = newIcons_save.createdElmImg("../to-do list/img/save.png", "save", newItem, newTask);
-        newTask.appendChild(newIcons_save);
-
-        let newIcons_ready = new AddControlButtons("../to-do list/img/task_icon.png", "completed", newItem, newTask);
-        newIcons_ready = newIcons_ready.createdElmImg("../to-do list/img/task_icon.png", "completed", newItem, newTask);
-        newTask.appendChild(newIcons_ready);
-
-        let newIcons_delete = new AddControlButtons("../to-do list/img/delete.png", "delete", newItem, newTask);
-        newIcons_delete = newIcons_delete.createdElmImg("../to-do list/img/delete.png", "delete", newItem, newTask);
-        newTask.appendChild(newIcons_delete);
-
-        //отчистим input
-        inputElement.value = "";
+    saveText (icons_save, newItem) {
+        icons_save.addEventListener('click', () => {
+            newItem.disabled = "disabled";
+            newItem.value = newItem.value;
+        });
     }
-});
 
-//ФУНКЦИИ
-function editText (icons_edit, newItem) {
-    icons_edit.addEventListener('click', () => {
-        newItem.disabled = "";
-        newItem.focus();
-    });
+    crossOutText (icons_ready, newItem){
+        icons_ready.addEventListener('click', () => {
+            if (newItem.style.textDecoration === styleCSS_forText) {
+                newItem.style.removeProperty(tagCSS);
+            } else {
+                newItem.style.setProperty(tagCSS, styleCSS_forText);
+            }
+        });
+    }
+
+    deleteText (icons_delete, newTask) {
+        icons_delete.addEventListener('click', () => {
+            newTask.remove();
+        });
+    }
 }
 
-function saveText (icons_save, newItem) {
-    icons_save.addEventListener('click', () => {
-        newItem.disabled = "disabled";
-        newItem.value = newItem.value;
-    });
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const inputElement = document.querySelector('.input');
+    const buttonElement = document.querySelector('.button');
+    const listTasks = document.querySelector('.taskList');
 
-function crossOutText (icons_ready, newItem){
-    icons_ready.addEventListener('click', () => {
-        if (newItem.style.textDecoration === styleCSS_forText) {
-            newItem.style.removeProperty(tagCSS);
-        } else {
-            newItem.style.setProperty(tagCSS, styleCSS_forText);
+    buttonElement.addEventListener("click", () => {
+        if (inputElement.value === "") {
+            inputElement.style.borderColor = "#800000";
+            alert("You didn't fill in this field!");
+        } 
+        else {
+            //контейнер для текста
+            const toDoList = new ToDoList("taskText");
+            const newTask = toDoList.createdElmDiv("taskText");
+            listTasks.appendChild(newTask);
+
+            //создание задания
+            const taskList = new TaskList("task", "text", inputElement.value, "disabled");
+            const newItem = taskList.createdElmTask("task", "text", inputElement.value, "disabled");
+            newTask.appendChild(newItem);
+
+            //создание кнопок управления
+            const controlButtons = new ControlButtons();
+            const newIcons_edit = controlButtons.createdElmImg("../to-do list/img/edit.png", "edit", newItem, newTask);
+            newTask.appendChild(newIcons_edit);
+
+            
+            const newIcons_save = controlButtons.createdElmImg("../to-do list/img/save.png", "save", newItem, newTask);
+            newTask.appendChild(newIcons_save);
+
+            
+            const newIcons_ready = controlButtons.createdElmImg("../to-do list/img/task_icon.png", "completed", newItem, newTask);
+            newTask.appendChild(newIcons_ready);
+
+            
+            const newIcons_delete = controlButtons.createdElmImg("../to-do list/img/delete.png", "delete", newItem, newTask);
+            newTask.appendChild(newIcons_delete);
+
+            //отчистим input
+            inputElement.value = "";
         }
     });
-}
 
-function deleteText (icons_delete, newTask) {
-    icons_delete.addEventListener('click', () => {
-        newTask.remove();
+    buttonElement.removeEventListener("click", () => {
+        if (inputElement.value === "") {
+            inputElement.style.borderColor = "#800000";
+            alert("You didn't fill in this field!");
+        } 
+        else {
+            //контейнер для текста
+            const toDoList = new ToDoList("taskText");
+            const newTask = toDoList.createdElmDiv("taskText");
+            listTasks.appendChild(newTask);
+
+            //создание задания
+            const taskList = new TaskList("task", "text", inputElement.value, "disabled");
+            const newItem = taskList.createdElmTask("task", "text", inputElement.value, "disabled");
+            newTask.appendChild(newItem);
+
+            //создание кнопок управления
+            const controlButtons = new ControlButtons();
+            const newIcons_edit = controlButtons.createdElmImg("../to-do list/img/edit.png", "edit", newItem, newTask);
+            newTask.appendChild(newIcons_edit);
+
+            
+            const newIcons_save = controlButtons.createdElmImg("../to-do list/img/save.png", "save", newItem, newTask);
+            newTask.appendChild(newIcons_save);
+
+            
+            const newIcons_ready = controlButtons.createdElmImg("../to-do list/img/task_icon.png", "completed", newItem, newTask);
+            newTask.appendChild(newIcons_ready);
+
+            
+            const newIcons_delete = controlButtons.createdElmImg("../to-do list/img/delete.png", "delete", newItem, newTask);
+            newTask.appendChild(newIcons_delete);
+
+            //отчистим input
+            inputElement.value = "";
+        }
     });
-}
+});
